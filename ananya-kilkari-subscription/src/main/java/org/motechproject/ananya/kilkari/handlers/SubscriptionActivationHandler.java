3bcc6c9acbca0cc9732a7d39745eb1f817f80c57
Srivatsa Katta <vatsa.katta@gmail.com>
2012-07-06 09:39:54 +0530

    Katta|000|Changed the omsm context path to OMSM

--- ananya-kilkari-subscription/src/main/java/org/motechproject/ananya/kilkari/handlers/SubscriptionActivationHandler.java
+++ ananya-kilkari-subscription/src/main/java/org/motechproject/ananya/kilkari/handlers/SubscriptionActivationHandler.java
@@ -30,7 +30,7 @@ public class SubscriptionActivationHandler {
     public void handleProcessSubscription(MotechEvent event) {
         SubscriptionActivationRequest subscriptionActivationRequest = (SubscriptionActivationRequest) event.getParameters().get("0");
         logger.info(String.format("Handling process subscription event for msisdn: %s, pack: %s, channel: %s", subscriptionActivationRequest.getMsisdn(), subscriptionActivationRequest.getPack(), subscriptionActivationRequest.getChannel()));
-            onMobileSubscriptionService.activateSubscription(subscriptionActivationRequest);
+        onMobileSubscriptionService.activateSubscription(subscriptionActivationRequest);
         subscriptionService.activationRequested(subscriptionActivationRequest.getSubscriptionId());
     }
 }
--- stubs/profiles/kilkari.profile
+++ stubs/profiles/kilkari.profile
@@ -1,6 +1,6 @@
 get|/omsm/activate => omsm|json|201
 get|/omsm/deactivate/:sid => omsm|json|200
-get|/omsm/ActivateSubscription => obd/slot1|json|200
+get|/OMSM/ActivateSubscription => obd/slot1|json|200
 
 
 

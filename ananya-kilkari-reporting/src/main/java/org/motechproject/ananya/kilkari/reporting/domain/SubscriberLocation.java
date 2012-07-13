ae57a9572675c6666d7a8714b88b344c0cfe65d3
Aravind M <arav.psgtech@gmail.com>
2012-07-13 15:47:15 +0530

    Aravind/Ashok | #00 | Added setters for json deserialization

--- ananya-kilkari-reporting/src/main/java/org/motechproject/ananya/kilkari/reporting/domain/SubscriberLocation.java
+++ ananya-kilkari-reporting/src/main/java/org/motechproject/ananya/kilkari/reporting/domain/SubscriberLocation.java
@@ -1,16 +1,19 @@
 package org.motechproject.ananya.kilkari.reporting.domain;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
+
 import java.io.Serializable;
 
 public class SubscriberLocation implements Serializable {
+    @JsonProperty
     private String district;
-
+    @JsonProperty
     private String block;
-
+    @JsonProperty
     private String panchayat;
 
     public SubscriberLocation() {
-
     }
 
     public SubscriberLocation(String district, String block, String panchayat) {
@@ -19,14 +22,17 @@ public class SubscriberLocation implements Serializable {
         this.panchayat = panchayat;
     }
 
+    @JsonIgnore
     public String getDistrict() {
         return district;
     }
 
+    @JsonIgnore
     public String getBlock() {
         return block;
     }
 
+    @JsonIgnore
     public String getPanchayat() {
         return panchayat;
     }

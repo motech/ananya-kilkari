9f8f87c2b4a71ce87e02849157b49e54fb06fb3e
Sharvani <sharvani.n.s@gmail.com>
2012-07-27 13:44:52 +0530

    Sharvani, Ashish | #1747| Sorting multiple messages based on week number.

--- ananya-kilkari-obd/src/main/java/org/motechproject/ananya/kilkari/obd/domain/NewMessageComparator.java
+++ ananya-kilkari-obd/src/main/java/org/motechproject/ananya/kilkari/obd/domain/NewMessageComparator.java
@@ -1,12 +1,33 @@
 package org.motechproject.ananya.kilkari.obd.domain;
 
 import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NewMessageComparator implements Comparator<CampaignMessage> {
+
+    private static final Pattern INT_PATTERN = Pattern.compile(".*?([0-9]+)$");
     @Override
     public int compare(CampaignMessage thisObject, CampaignMessage otherObject) {
         if (thisObject.hasFailed() && !otherObject.hasFailed()) return -1;
         if (otherObject.hasFailed() && !thisObject.hasFailed()) return 1;
-        return thisObject.getDncRetryCount() > otherObject.getDncRetryCount() ? -1 : (thisObject.getDncRetryCount() == otherObject.getDncRetryCount() ? 0 : 1);
+        if(thisObject.getDncRetryCount() > otherObject.getDncRetryCount()) return -1;
+        if(thisObject.getDncRetryCount() < otherObject.getDncRetryCount()) return 1;
+        return compareMessageId(thisObject, otherObject);
+    }
+
+    private int compareMessageId(CampaignMessage thisObject, CampaignMessage otherObject) {
+        int thisMessageId = findWeekNumber(thisObject.getMessageId());
+        int otherMessageId = findWeekNumber(otherObject.getMessageId());
+        return thisMessageId < otherMessageId ? -1 : thisMessageId > otherMessageId ? 1 : 0;
+    }
+
+
+    private int findWeekNumber(String messageId) {
+        Matcher matcher = INT_PATTERN.matcher(messageId);
+        if(!matcher.matches()) {
+            throw new IllegalArgumentException(String.format("Wrong format for messageId: %s", messageId));
+        }
+        return Integer.parseInt(matcher.group(1));
     }
 }
--- ananya-kilkari-obd/src/test/java/org/motechproject/ananya/kilkari/obd/domain/NewMessageComparatorTest.java
+++ ananya-kilkari-obd/src/test/java/org/motechproject/ananya/kilkari/obd/domain/NewMessageComparatorTest.java
@@ -1,7 +1,9 @@
 package org.motechproject.ananya.kilkari.obd.domain;
 
 import org.joda.time.DateTime;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,6 +12,10 @@ import java.util.List;
 import static junit.framework.Assert.assertEquals;
 
 public class NewMessageComparatorTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
     public void shouldReturnObjectWithAFailureOverAnObjectWithoutOnComparison() {
         CampaignMessage erroredCampaignMessage = new CampaignMessage();
@@ -44,5 +50,81 @@ public class NewMessageComparatorTest {
 
         assertEquals(campaignMessageWithHigherDNCRetry, campaignMessageList.get(0));
         assertEquals(campaignMessageWithLowerDNCRetry, campaignMessageList.get(1));
+    }
+
+    @Test
+    public void shouldReturnAnObjectWithLowerCampaignMessageWeek() {
+        final CampaignMessage campaignMessageForWeek1 = new CampaignMessage("subscripitonId", "WEEK1", "1234567890", "IDEA", DateTime.now().minusWeeks(1));
+        final CampaignMessage campaignMessageForWeek2 = new CampaignMessage("subscripitonId", "WEEK2", "1234567890", "IDEA", DateTime.now().minusWeeks(1));
+        final CampaignMessage campaignMessageForWeek3 = new CampaignMessage("subscripitonId", "WEEK3", "1234567890", "IDEA", DateTime.now().minusWeeks(1));
+
+        List<CampaignMessage> campaignMessages = new ArrayList<CampaignMessage>() {{
+            add(campaignMessageForWeek3);
+            add(campaignMessageForWeek2);
+            add(campaignMessageForWeek1);
+        }};
+
+        Collections.sort(campaignMessages, new NewMessageComparator());
+
+        assertEquals(campaignMessageForWeek1, campaignMessages.get(0));
+        assertEquals(campaignMessageForWeek2, campaignMessages.get(1));
+        assertEquals(campaignMessageForWeek3, campaignMessages.get(2));
+    }
+
+    @Test
+    public void shouldThrowExceptionIfMessageIdIsNotInCorrectFormat() {
+        final CampaignMessage campaignMessageWithInvalidMessageID = new CampaignMessage("subscripitonId1", "WEEK", "1234567890", "IDEA", DateTime.now().minusWeeks(1));
+        final CampaignMessage campaignMessage = new CampaignMessage("subscripitonId2", "WEEK12", "1234567891", "IDEA", DateTime.now().minusWeeks(1));
+
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Wrong format for messageId: WEEK");
+
+        List<CampaignMessage> campaignMessages = new ArrayList<CampaignMessage>() {{
+            add(campaignMessage);
+            add(campaignMessageWithInvalidMessageID);
+        }};
+
+        Collections.sort(campaignMessages, new NewMessageComparator());
+    }
+
+    @Test
+    public void shouldReturnAnObjectWithDNCBeforeNew_AndHigherRetryCount_AndLowerCampaignMessageWeek() {
+        final CampaignMessage campaignMessageWithPriority5 = new CampaignMessage("subscripitonId1", "WEEK12", "1234567890", "IDEA", DateTime.now().minusWeeks(1));
+        campaignMessageWithPriority5.setStatusCode(CampaignMessageStatus.NEW);
+
+        final CampaignMessage campaignMessageWithPriority4 = new CampaignMessage("subscripitonId2", "WEEK1", "1234567891", "IDEA", DateTime.now().minusWeeks(1));
+        campaignMessageWithPriority4.setStatusCode(CampaignMessageStatus.NEW);
+
+        final CampaignMessage campaignMessageWithPriority2 = new CampaignMessage("subscripitonId3", "WEEK7", "1234567893", "IDEA", DateTime.now().minusWeeks(1));
+        campaignMessageWithPriority2.setStatusCode(CampaignMessageStatus.DNC);
+        campaignMessageWithPriority2.markSent();
+        campaignMessageWithPriority2.markSent();
+
+        final CampaignMessage campaignMessageWithPriority3 = new CampaignMessage("subscripitonId4", "WEEK12", "1234567894", "IDEA", DateTime.now().minusWeeks(1));
+        campaignMessageWithPriority3.setStatusCode(CampaignMessageStatus.DNC);
+        campaignMessageWithPriority3.markSent();
+        campaignMessageWithPriority3.markSent();
+
+        final CampaignMessage campaignMessageWithPriority1 = new CampaignMessage("subscripitonId5", "WEEK6", "1234567895", "IDEA", DateTime.now().minusWeeks(1));
+        campaignMessageWithPriority1.setStatusCode(CampaignMessageStatus.DNC);
+        campaignMessageWithPriority1.markSent();
+        campaignMessageWithPriority1.markSent();
+        campaignMessageWithPriority1.markSent();
+
+        List<CampaignMessage> campaignMessages = new ArrayList<CampaignMessage>() {{
+            add(campaignMessageWithPriority2);
+            add(campaignMessageWithPriority5);
+            add(campaignMessageWithPriority4);
+            add(campaignMessageWithPriority1);
+            add(campaignMessageWithPriority3);
+        }};
+
+        Collections.sort(campaignMessages, new NewMessageComparator());
+
+        assertEquals(campaignMessageWithPriority1, campaignMessages.get(0));
+        assertEquals(campaignMessageWithPriority2, campaignMessages.get(1));
+        assertEquals(campaignMessageWithPriority3, campaignMessages.get(2));
+        assertEquals(campaignMessageWithPriority4, campaignMessages.get(3));
+        assertEquals(campaignMessageWithPriority5, campaignMessages.get(4));
     }
 }

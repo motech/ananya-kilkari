db452d9719e2d38fd482d535b9f113775d987649
Sharvani <sharvani.n.s@gmail.com>
2012-07-30 18:47:24 +0530

    Sharvani | #1747 | Added sorting by week functionality for retry messages.

--- ananya-kilkari-obd/src/main/java/org/motechproject/ananya/kilkari/obd/domain/RetryMessageComparator.java
+++ ananya-kilkari-obd/src/main/java/org/motechproject/ananya/kilkari/obd/domain/RetryMessageComparator.java
@@ -1,10 +1,31 @@
 package org.motechproject.ananya.kilkari.obd.domain;
 
 import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RetryMessageComparator implements Comparator<CampaignMessage> {
+
+    private static final Pattern INT_PATTERN = Pattern.compile(".*?([0-9]+)$");
+
     @Override
     public int compare(CampaignMessage thisObject, CampaignMessage otherObject) {
-        return thisObject.getDnpRetryCount() > otherObject.getDnpRetryCount() ? -1 : (thisObject.getDnpRetryCount() == otherObject.getDnpRetryCount() ? 0 : 1);
+        if(thisObject.getDnpRetryCount() > otherObject.getDnpRetryCount()) return -1;
+        if(thisObject.getDnpRetryCount() < otherObject.getDnpRetryCount()) return 1;
+        return compareMessageId(thisObject, otherObject);
+    }
+
+    private int compareMessageId(CampaignMessage thisObject, CampaignMessage otherObject) {
+        int thisMessageId = findWeekNumber(thisObject.getMessageId());
+        int otherMessageId = findWeekNumber(otherObject.getMessageId());
+        return thisMessageId < otherMessageId ? -1 : thisMessageId > otherMessageId ? 1 : 0;
+    }
+
+    private int findWeekNumber(String messageId) {
+        Matcher matcher = INT_PATTERN.matcher(messageId);
+        if(!matcher.matches()) {
+            throw new IllegalArgumentException(String.format("Wrong format for messageId: %s", messageId));
+        }
+        return Integer.parseInt(matcher.group(1));
     }
 }

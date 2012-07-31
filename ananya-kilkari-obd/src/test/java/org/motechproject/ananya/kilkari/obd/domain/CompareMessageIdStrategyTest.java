af4b828411c3397f9d06e65afd93228125929d53
Sharvani <sharvani.n.s@gmail.com>
2012-07-31 10:12:05 +0530

    Sharvani | #1747 | Refactoring Campaign Message comparators.

---
+++ ananya-kilkari-obd/src/test/java/org/motechproject/ananya/kilkari/obd/domain/CompareMessageIdStrategyTest.java
@@ -0,0 +1,18 @@
+package org.motechproject.ananya.kilkari.obd.domain;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class CompareMessageIdStrategyTest {
+
+    @Test
+    public void shouldReturnAnObjectWithLowerCampaignMessageWeek() {
+        final CampaignMessage campaignMessageForWeek1 = new CampaignMessage("subscripitonId1", "WEEK1", "1234567890", "IDEA", DateTime.now().minusWeeks(1));
+        final CampaignMessage campaignMessageForWeek3 = new CampaignMessage("subscripitonId3", "WEEK3", "1234567890", "IDEA", DateTime.now().minusWeeks(1));
+
+        int compare = CompareMessageIdStrategy.compare(campaignMessageForWeek1, campaignMessageForWeek3);
+        assertEquals(-1, compare);
+    }
+}

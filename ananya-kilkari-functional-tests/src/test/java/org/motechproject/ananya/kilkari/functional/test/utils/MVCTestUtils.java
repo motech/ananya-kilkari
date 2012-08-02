52fe38e8b8c29c27f1a0dc65962c4e5b4b4103ad
Vandana <vandanag@vandanag-Latitude-E6510.(none)>
2012-08-02 16:09:35 +0530

    Revert "Katta|000|Fixed the maven module folder name to be in sync with module name"
    This reverts commit 0ee4e0383fb59104d65a16c489550cf7c5c82064.

---
+++ ananya-kilkari-functional-tests/src/test/java/org/motechproject/ananya/kilkari/functional/test/utils/MVCTestUtils.java
@@ -0,0 +1,25 @@
+package org.motechproject.ananya.kilkari.functional.test.utils;
+
+import org.junit.Ignore;
+import org.motechproject.ananya.kilkari.web.KilkariExceptionResolver;
+import org.motechproject.ananya.kilkari.web.KilkariViewResolver;
+import org.motechproject.ananya.kilkari.web.interceptors.KilkariChannelInterceptor;
+import org.springframework.test.web.server.MockMvc;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+import org.springframework.test.web.server.setup.StandaloneMockMvcBuilder;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.util.Arrays;
+
+@Ignore
+public class MVCTestUtils {
+
+    public static MockMvc mockMvc(Object controller) {
+        StandaloneMockMvcBuilder mockMvcBuilder = MockMvcBuilders.standaloneSetup(controller)
+                .addInterceptors(new KilkariChannelInterceptor())
+                .setViewResolvers(new KilkariViewResolver());
+
+        mockMvcBuilder.setHandlerExceptionResolvers(Arrays.asList(new HandlerExceptionResolver[]{new KilkariExceptionResolver()}));
+        return mockMvcBuilder.build();
+    }
+}

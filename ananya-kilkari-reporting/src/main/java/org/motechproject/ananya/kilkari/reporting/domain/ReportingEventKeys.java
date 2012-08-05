387c9c41e850b12951315134489c3c4bef241d3d
Aravind M <arav.psgtech@gmail.com>
2012-08-05 16:34:04 +0530

    Aravind | #00 | Minor cleanup.

--- ananya-kilkari-reporting/src/main/java/org/motechproject/ananya/kilkari/reporting/domain/ReportingEventKeys.java
+++
@@ -1,8 +0,0 @@
-package org.motechproject.ananya.kilkari.reporting.domain;
-
-public class ReportingEventKeys {
-    public static final String REPORT_SUBSCRIPTION_CREATION = "org.motechproject.ananya.kilkari.domain.report.create.subscription";
-    public static final String REPORT_SUBSCRIPTION_STATE_CHANGE = "org.motechproject.ananya.kilkari.domain.report.subscription.state.change";
-    public static final String REPORT_CAMPAIGN_MESSAGE_DELIVERY_STATUS = "org.motechproject.ananya.kilkari.domain.report.campaign.message.delivery.status";
-    public static final String REPORT_SUBSCRIBER_DETAILS_UPDATE = "org.motechproject.ananya.kilkari.domain.report.subscriber.details.update";
-}

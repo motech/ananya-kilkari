925a41b175ec7aff65f3b31bb942ca7d6126241c
Ashish Mishra <ashishkm@thoughtworks.com>
2012-08-10 15:10:53 +0530

    Ensuring that quartz scheduler thread is fired immediately after we move ahead in time.

--- ananya-kilkari-functional-tests/src/test/java/org/motechproject/ananya/kilkari/functional/test/domain/Time.java
+++ ananya-kilkari-functional-tests/src/test/java/org/motechproject/ananya/kilkari/functional/test/domain/Time.java
@@ -2,11 +2,29 @@ package org.motechproject.ananya.kilkari.functional.test.domain;
 
 import org.joda.time.DateTime;
 import org.motechproject.ananya.kilkari.functional.test.utils.FakeTimeUtils;
+import org.quartz.SchedulerException;
+import org.quartz.core.QuartzScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Time {
+
+    @Autowired
+    private SchedulerFactoryBean schedulerFactoryBean;
+
+
     public void isMovedToFuture(DateTime dateTime) {
         FakeTimeUtils.moveToFutureTime(dateTime);
+        forceRunQuartzScheduler();
+    }
+
+    private void forceRunQuartzScheduler() {
+        try {
+            schedulerFactoryBean.getScheduler().resumeAll();
+        } catch (SchedulerException e) {
+            new RuntimeException(e);
+        }
     }
 }

44f13c19c080d7a7edf7772cba1906f8b39493fd
f1codz <imdad.ahmed@gmail.com>
2012-09-14 16:35:41 +0530

    Ashok, Imdad | #2075 | Added missing spring annotations.

--- ananya-kilkari-obd/src/main/java/org/motechproject/ananya/kilkari/obd/service/InvalidOBDEntriesHandler.java
+++ ananya-kilkari-obd/src/main/java/org/motechproject/ananya/kilkari/obd/service/InvalidOBDEntriesHandler.java
@@ -1,18 +1,24 @@
 package org.motechproject.ananya.kilkari.obd.service;
 
+import org.apache.log4j.Logger;
 import org.motechproject.ananya.kilkari.obd.domain.InvalidCallRecord;
 import org.motechproject.ananya.kilkari.obd.service.request.InvalidOBDRequestEntries;
 import org.motechproject.ananya.kilkari.obd.service.request.InvalidOBDRequestEntry;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.event.annotations.MotechListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class InvalidOBDEntriesHandler {
 
+    Logger logger = Logger.getLogger(InvalidOBDEntriesHandler.class);
     private InvalidOBDEntriesService invalidOBDEntriesService;
 
+    @Autowired
     public InvalidOBDEntriesHandler(InvalidOBDEntriesService invalidOBDEntriesService) {
         this.invalidOBDEntriesService = invalidOBDEntriesService;
     }
@@ -21,6 +27,8 @@ public class InvalidOBDEntriesHandler {
     public void handleInvalidCallRecordsRequest(MotechEvent event) {
         InvalidOBDRequestEntries invalidOBDRequestEntries = (InvalidOBDRequestEntries) event.getParameters().get("0");
         List<InvalidCallRecord> invalidCallRecords = mapToInvalidCallRecord(invalidOBDRequestEntries);
+
+        logger.info("Handling invalid obd request entries");
         invalidOBDEntriesService.processInvalidCallRecords(invalidCallRecords);
     }
 

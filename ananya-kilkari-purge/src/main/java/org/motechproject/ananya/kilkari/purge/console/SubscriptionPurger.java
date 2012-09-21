4fdfac7de9b914113040f42f46ce88f7debe74f7
Aravind M <arav.psgtech@gmail.com>
2012-09-21 12:36:42 +0530

    Ashok/Aravind | #1774 | Setting the profile to production profile for the SubscriptionPurger tool.

--- ananya-kilkari-purge/src/main/java/org/motechproject/ananya/kilkari/purge/console/SubscriptionPurger.java
+++ ananya-kilkari-purge/src/main/java/org/motechproject/ananya/kilkari/purge/console/SubscriptionPurger.java
@@ -1,7 +1,7 @@
 package org.motechproject.ananya.kilkari.purge.console;
 
-import org.motechproject.ananya.kilkari.purge.service.PurgeOrchestrator;
 import org.motechproject.ananya.kilkari.purge.exception.WrongNumberArgsException;
+import org.motechproject.ananya.kilkari.purge.service.PurgeOrchestrator;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
@@ -12,9 +12,16 @@ public class SubscriptionPurger {
     public static void main(String[] args) throws WrongNumberArgsException, IOException {
         validateArguments(args);
         String filePath = args[0];
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML);
-        PurgeOrchestrator purgeOrchestrator = (PurgeOrchestrator) context.getBean("purgeOrchestrator");
+        PurgeOrchestrator purgeOrchestrator = getPurgeOrchestrator();
         purgeOrchestrator.purgeSubscriptionData(filePath);
+    }
+
+    private static PurgeOrchestrator getPurgeOrchestrator() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext();
+        context.getEnvironment().setActiveProfiles("production");
+        context.setConfigLocation(APPLICATION_CONTEXT_XML);
+        context.refresh();
+        return (PurgeOrchestrator) context.getBean("purgeOrchestrator");
     }
 
     private static void validateArguments(String[] args) throws WrongNumberArgsException {

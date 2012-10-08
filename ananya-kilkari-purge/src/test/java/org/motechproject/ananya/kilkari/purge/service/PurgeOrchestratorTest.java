3ba1a0aa814ce2671547098d558c0409360de492
Ashish Mishra <ashishkm@thoughtworks.com>
2012-10-08 18:34:02 +0530

    Ashish | #1774 | Fixing orchestrator to ignore empty lines in the file. Also trimming msisdn before processing.

--- ananya-kilkari-purge/src/test/java/org/motechproject/ananya/kilkari/purge/service/PurgeOrchestratorTest.java
+++ ananya-kilkari-purge/src/test/java/org/motechproject/ananya/kilkari/purge/service/PurgeOrchestratorTest.java
@@ -12,6 +12,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
 @RunWith(MockitoJUnitRunner.class)
 public class PurgeOrchestratorTest {
     @Mock
@@ -27,20 +29,74 @@ public class PurgeOrchestratorTest {
     public void setUp() throws IOException {
         purgeOrchestrator = new PurgeOrchestrator(kilkariPurgeService, quartzSchedulerPurgeService);
         tempFile = File.createTempFile("tmp", "txt");
+        tempFile.deleteOnExit();
         filePath = tempFile.getAbsolutePath();
     }
 
     @Test
     public void shouldPurgeSubscription() throws IOException {
-        String msisdn = "1234567890";
+        String msisdn1 = "1234567890";
+        String msisdn2 = "1234567891";
         FileWriter fileWriter = new FileWriter(tempFile);
-        fileWriter.write(msisdn);
+        fileWriter.write(msisdn1);
+        fileWriter.write("\n" + msisdn2);
         fileWriter.close();
 
         purgeOrchestrator.purgeSubscriptionData(filePath);
 
         InOrder inOrder = Mockito.inOrder(quartzSchedulerPurgeService, kilkariPurgeService);
-        inOrder.verify(quartzSchedulerPurgeService).deleteFor(msisdn);
-        inOrder.verify(kilkariPurgeService).purge(msisdn);
+        inOrder.verify(quartzSchedulerPurgeService).deleteFor(msisdn1);
+        inOrder.verify(kilkariPurgeService).purge(msisdn1);
+        inOrder.verify(quartzSchedulerPurgeService).deleteFor(msisdn2);
+        inOrder.verify(kilkariPurgeService).purge(msisdn2);
+
+        verifyNoMoreInteractions(quartzSchedulerPurgeService);
+        verifyNoMoreInteractions(kilkariPurgeService);
+    }
+
+
+    @Test
+    public void shouldIgnoreBlankLinesWhileReadingFromTheFile() throws IOException {
+        String msisdn1 = "1234567890";
+        String msisdn2 = "1234567891";
+        FileWriter fileWriter = new FileWriter(tempFile);
+        fileWriter.write("");
+        fileWriter.write("\n" + msisdn1);
+        fileWriter.write("\n    ");
+        fileWriter.write("\n" + msisdn2);
+        fileWriter.write("\n");
+        fileWriter.close();
+
+        purgeOrchestrator.purgeSubscriptionData(filePath);
+
+        InOrder inOrder = Mockito.inOrder(quartzSchedulerPurgeService, kilkariPurgeService);
+        inOrder.verify(quartzSchedulerPurgeService).deleteFor(msisdn1);
+        inOrder.verify(kilkariPurgeService).purge(msisdn1);
+        inOrder.verify(quartzSchedulerPurgeService).deleteFor(msisdn2);
+        inOrder.verify(kilkariPurgeService).purge(msisdn2);
+
+        verifyNoMoreInteractions(quartzSchedulerPurgeService);
+        verifyNoMoreInteractions(kilkariPurgeService);
+    }
+
+    @Test
+    public void shouldTrimMsidnWhileReadingFromTheFile() throws IOException {
+        String msisdn1 = "1234567890";
+        String msisdn2 = "1234567891";
+        FileWriter fileWriter = new FileWriter(tempFile);
+        fileWriter.write("    " + msisdn1 + "    ");
+        fileWriter.write("\n    " + msisdn2);
+        fileWriter.close();
+
+        purgeOrchestrator.purgeSubscriptionData(filePath);
+
+        InOrder inOrder = Mockito.inOrder(quartzSchedulerPurgeService, kilkariPurgeService);
+        inOrder.verify(quartzSchedulerPurgeService).deleteFor(msisdn1);
+        inOrder.verify(kilkariPurgeService).purge(msisdn1);
+        inOrder.verify(quartzSchedulerPurgeService).deleteFor(msisdn2);
+        inOrder.verify(kilkariPurgeService).purge(msisdn2);
+
+        verifyNoMoreInteractions(quartzSchedulerPurgeService);
+        verifyNoMoreInteractions(kilkariPurgeService);
     }
 }

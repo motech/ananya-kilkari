4b71d774cd3cea075772b0fdc6c4fda6cfe7996d
Aravind M <arav.psgtech@gmail.com>
2012-10-26 17:35:27 +0530

    Aravind/Sushmitha | #2098 | Checking if subscription exists explicitly on validation.

--- ananya-kilkari-web/src/main/java/org/motechproject/ananya/kilkari/web/validators/CallbackRequestValidator.java
+++ ananya-kilkari-web/src/main/java/org/motechproject/ananya/kilkari/web/validators/CallbackRequestValidator.java
@@ -3,13 +3,13 @@ package org.motechproject.ananya.kilkari.web.validators;
 import org.motechproject.ananya.kilkari.domain.CallbackAction;
 import org.motechproject.ananya.kilkari.domain.CallbackStatus;
 import org.motechproject.ananya.kilkari.factory.SubscriptionStateHandlerFactory;
+import org.motechproject.ananya.kilkari.obd.domain.PhoneNumber;
 import org.motechproject.ananya.kilkari.obd.service.validator.Errors;
 import org.motechproject.ananya.kilkari.request.CallbackRequestWrapper;
 import org.motechproject.ananya.kilkari.subscription.domain.Operator;
 import org.motechproject.ananya.kilkari.subscription.domain.Subscription;
 import org.motechproject.ananya.kilkari.subscription.domain.SubscriptionStatus;
 import org.motechproject.ananya.kilkari.subscription.service.SubscriptionService;
-import org.motechproject.ananya.kilkari.obd.domain.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -45,27 +45,33 @@ public class CallbackRequestValidator {
         final String requestAction = callbackRequestWrapper.getAction();
 
         if (subscriptionStateHandlerFactory.getHandler(callbackRequestWrapper) == null) {
-            errors.add(String.format("Invalid status %s for action %s for subscription %s", requestStatus, requestAction,callbackRequestWrapper.getSubscriptionId()));
+            errors.add(String.format("Invalid status %s for action %s for subscription %s", requestStatus, requestAction, callbackRequestWrapper.getSubscriptionId()));
         }
 
         Subscription subscription = subscriptionService.findBySubscriptionId(callbackRequestWrapper.getSubscriptionId());
+        if (subscription == null) {
+            errors.add(String.format("No subscription for subscriptionId : %s", callbackRequestWrapper.getSubscriptionId()));
+            return errors;
+        }
+
+
         if (CallbackAction.REN.name().equals(requestAction)) {
             final SubscriptionStatus subscriptionStatus = subscription.getStatus();
-            if(!subscriptionStatus.canRenew()) {
-                errors.add(String.format("Cannot renew. Subscription %s in %s status", callbackRequestWrapper.getSubscriptionId(),subscriptionStatus));
+            if (!subscriptionStatus.canRenew()) {
+                errors.add(String.format("Cannot renew. Subscription %s in %s status", callbackRequestWrapper.getSubscriptionId(), subscriptionStatus));
             }
         }
 
         if (CallbackAction.DCT.name().equals(requestAction) && CallbackStatus.BAL_LOW.name().equals(requestStatus)) {
             final SubscriptionStatus subscriptionStatus = subscription.getStatus();
             if (!subscriptionStatus.canDeactivateOnRenewal())
-                errors.add(String.format("Cannot deactivate on renewal. Subscription %s in %s status", callbackRequestWrapper.getSubscriptionId(),subscriptionStatus));
+                errors.add(String.format("Cannot deactivate on renewal. Subscription %s in %s status", callbackRequestWrapper.getSubscriptionId(), subscriptionStatus));
         }
 
         if (CallbackAction.ACT.name().equals(requestAction) && (CallbackStatus.SUCCESS.name().equals(requestStatus) || CallbackStatus.BAL_LOW.name().equals(requestStatus))) {
             final SubscriptionStatus subscriptionStatus = subscription.getStatus();
             if (!subscriptionStatus.canActivate())
-                errors.add(String.format("Cannot activate. Subscription %s in %s status", callbackRequestWrapper.getSubscriptionId(),subscriptionStatus));
+                errors.add(String.format("Cannot activate. Subscription %s in %s status", callbackRequestWrapper.getSubscriptionId(), subscriptionStatus));
         }
 
         return errors;
@@ -75,19 +81,19 @@ public class CallbackRequestValidator {
     private void validateOperator(CallbackRequestWrapper callbackRequestWrapper, Errors errors) {
         String operator = callbackRequestWrapper.getOperator();
         if (!Operator.isValid(operator))
-            errors.add(String.format("Invalid operator %s for subscription %s", callbackRequestWrapper.getOperator(),callbackRequestWrapper.getSubscriptionId()));
+            errors.add(String.format("Invalid operator %s for subscription %s", callbackRequestWrapper.getOperator(), callbackRequestWrapper.getSubscriptionId()));
     }
 
     private void validateMsisdn(CallbackRequestWrapper callbackRequestWrapper, Errors errors) {
         String msisdn = callbackRequestWrapper.getMsisdn();
         if (PhoneNumber.isNotValid(msisdn))
-            errors.add(String.format("Invalid msisdn %s for subscription id %s", msisdn,callbackRequestWrapper.getSubscriptionId()));
+            errors.add(String.format("Invalid msisdn %s for subscription id %s", msisdn, callbackRequestWrapper.getSubscriptionId()));
     }
 
     private boolean validateCallbackAction(CallbackRequestWrapper callbackRequestWrapper, Errors errors) {
         String callbackAction = callbackRequestWrapper.getAction();
         if (!CallbackAction.isValid(callbackAction)) {
-            errors.add(String.format("Invalid callbackAction %s  for subscription %s", callbackAction,callbackRequestWrapper.getSubscriptionId()));
+            errors.add(String.format("Invalid callbackAction %s  for subscription %s", callbackAction, callbackRequestWrapper.getSubscriptionId()));
             return false;
         }
         return true;
@@ -96,7 +102,7 @@ public class CallbackRequestValidator {
     private boolean validateCallbackStatus(CallbackRequestWrapper callbackRequestWrapper, Errors errors) {
         String callbackStatus = callbackRequestWrapper.getStatus();
         if (!CallbackStatus.isValid(callbackStatus)) {
-            errors.add(String.format("Invalid callbackStatus %s for subscription %s", callbackStatus,callbackRequestWrapper.getSubscriptionId()));
+            errors.add(String.format("Invalid callbackStatus %s for subscription %s", callbackStatus, callbackRequestWrapper.getSubscriptionId()));
             return false;
         }
         return true;

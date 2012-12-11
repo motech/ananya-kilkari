a3fcfcf53653c67f8d5f289bb889c3b847118b3f
Ashish Mishra <ashishkm@thoughtworks.com>
2012-12-11 16:16:18 +0530

    #105 | Ashish | Changing mock mvc utils for functional tests

--- ananya-kilkari-functional-tests/src/test/java/org/motechproject/ananya/kilkari/functional/test/utils/MVCTestUtils.java
+++ ananya-kilkari-functional-tests/src/test/java/org/motechproject/ananya/kilkari/functional/test/utils/MVCTestUtils.java
@@ -4,6 +4,9 @@ import org.junit.Ignore;
 import org.motechproject.ananya.kilkari.web.KilkariExceptionResolver;
 import org.motechproject.ananya.kilkari.web.KilkariViewResolver;
 import org.motechproject.ananya.kilkari.web.interceptors.KilkariChannelInterceptor;
+import org.motechproject.web.message.converters.CSVHttpMessageConverter;
+import org.motechproject.web.message.converters.CustomJaxb2RootElementHttpMessageConverter;
+import org.motechproject.web.message.converters.CustomMappingJacksonHttpMessageConverter;
 import org.springframework.test.web.server.MockMvc;
 import org.springframework.test.web.server.setup.MockMvcBuilders;
 import org.springframework.test.web.server.setup.StandaloneMockMvcBuilder;
@@ -18,7 +21,7 @@ public class MVCTestUtils {
         StandaloneMockMvcBuilder mockMvcBuilder = MockMvcBuilders.standaloneSetup(controller)
                 .addInterceptors(new KilkariChannelInterceptor())
                 .setViewResolvers(new KilkariViewResolver());
-
+        mockMvcBuilder.setMessageConverters(new CustomMappingJacksonHttpMessageConverter(), new CustomJaxb2RootElementHttpMessageConverter(), new CSVHttpMessageConverter());
         mockMvcBuilder.setHandlerExceptionResolvers(Arrays.asList(new HandlerExceptionResolver[]{new KilkariExceptionResolver()}));
         return mockMvcBuilder.build();
     }

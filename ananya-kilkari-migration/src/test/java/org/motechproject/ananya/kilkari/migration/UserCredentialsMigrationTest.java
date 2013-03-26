d1686d44dd0a3fe892a4bc944db394d47657d724
vishnu karthik <karthikl@thoughtworks.com>
2013-03-26 18:30:38 +0530

    Vishnu, Ashok | #2265 | Adding a new migration for the admin password change

--- ananya-kilkari-migration/src/test/java/org/motechproject/ananya/kilkari/migration/UserCredentialsMigrationTest.java
+++ ananya-kilkari-migration/src/test/java/org/motechproject/ananya/kilkari/migration/UserCredentialsMigrationTest.java
@@ -1,35 +1,32 @@
-package db.migration;
+package org.motechproject.ananya.kilkari.migration;
+
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.motechproject.ananya.kilkari.admin.domain.AdminUser;
 import org.motechproject.ananya.kilkari.admin.repository.AllAdminUsers;
 
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertTrue;
 
-@RunWith(MockitoJUnitRunner.class)
-public class V1_1__AdminUserCreationTest {
-
-    private V1_1__AdminUserCreation adminMigration;
+public class UserCredentialsMigrationTest {
+    private UserCredentialsMigration userCredentialsMigration;
     private AllAdminUsers adminUserRepository;
 
     @Before
     @After
     public void setUp() throws Exception {
-        adminMigration = new V1_1__AdminUserCreation();
-        adminUserRepository = adminMigration.getAdminUserRepository();
+        userCredentialsMigration = new UserCredentialsMigration();
+        adminUserRepository = userCredentialsMigration.getAdminUserRepository();
         adminUserRepository.removeAll();
     }
 
     @Test
-    public void shouldCreateNewAdminUser() throws Exception {
-        adminMigration.migrate(null);
+    public void shouldCreateOrUpdateAnUser() throws Exception {
+        userCredentialsMigration.migrate();
 
         List<AdminUser> adminUsers = adminUserRepository.getAll();
         assertEquals(1, adminUsers.size());
